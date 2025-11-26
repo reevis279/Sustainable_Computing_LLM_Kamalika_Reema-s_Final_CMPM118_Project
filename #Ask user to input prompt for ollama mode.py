42c800863110d6ce0da30220d3deac8783a4c13c
kamaDe25 <kamalikade25@gmail.com>
2025-11-26 14:47:50 -0800

    learning how to get ollama to process query

--- #Ask user to input prompt for ollama mode.py
+++ #Ask user to input prompt for ollama mode.py
@@ -6,8 +6,14 @@ prompt = input("Enter your prompt: ")
 #print("This is prompt:", prompt)
 
 #Get this application to check if ollama is installed.
+#Probably have to use subprocess to run a command line instruction to check if ollama is installed.
+
 
 #If ollama isn't install then tell user to install ollama.
+#Probably have to use a config file to install ollama
+#Probably have to use docker to use config file
+
+#run quen 2.5-coder 3b model in lmstudio config file here
 
 #If ollama is installed, ask it the prompt.
 import ollama #error here, even though I have ollama installed
